bf8275a78b24c24275c50b05d3d5f809d0dd564d
Ambrose Chua <ambrose.chua.cm@gmail.com>
2015-10-14 15:32:51 +0800

    #HowToDeprecateYourClassesYouWrote1SecondAgo

---
+++ src/io/makerforce/undefined/model/JSONLibrary.java
@@ -0,0 +1,12 @@
+package io.makerforce.undefined.model;
+
+import org.json.JSONObject;
+
+@Deprecated
+public class JSONLibrary extends Library {
+
+    public JSONLibrary(JSONObject o) {
+        super(o);
+    }
+
+}
